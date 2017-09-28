1f548be4fca96dbba6488dcdb76ae7eda04e1ac6
Erdogan Ozkoca <erdogan.ozkoca@red-gate.com>
2017-09-28 09:43:59 +0100

    Update state with product

--- app2/src/App.js
+++ app2/src/App.js
@@ -4,6 +4,10 @@ import './App.css'
 import data from './data.js'
 import Products from './Products.js'
 
+const addProductToState = (name, description) => (prevState, props) => ({
+  products: [{name: name, description: description} ,...prevState.products  ]
+})
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -12,13 +16,20 @@ class App extends Component {
     };
   }
 
+  addProduct(event){
+    const target = event.target;
+    const name = target.productName.value;
+    const description = target.description.value;
+    this.setState(addProductToState(name, description));
+    event.preventDefault();
+  }
   render() {
     return <div className="App">
       <div className="App-header">
         <h2>Kata 2- Add and remove objects</h2>
       </div>
       <div className='add-product'>
-        <form>
+        <form onSubmit={e => this.addProduct(e)}>
           <p>Product Name</p>
           <input type="text" name="productName" />
           <p>Description</p>
